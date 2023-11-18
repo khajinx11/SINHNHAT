962021bae8360e9a44272f5a755d2b48ed841147
khajinx11 <116270904+khajinx11@users.noreply.github.com>
2023-11-19 00:29:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,60 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+  // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+  // 每句话尽量不要超过15个字,不然展示效果可能不太好
+  texts: [
+    "Ê", //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    "Gái", // 同上...
+    "Giờ tao cũng không hiểu",
+    "3h giờ sáng tao không đi ngủ",
+    "Mà bật máy làm cái xàm quần này",
+    "Xong ngồi chỉnh code ",
+    "Từ tiếng Trung",
+    "Qua tiếng Việt",
+    "Rồi qua lại Tiếng Anh",
+    "Đê làm cái này cho mày",
+    "Mà thôi kệ",
+    "Coi như ban phước",
+    "Chúc tuổi 20",
+    "Méo xu như tuổi 19 của tao :))",
+    "Vậy nha",
+    "Hết òi",
+    "....",
+    "...",
+    "..",
+    ".",
+    "Hết thiệt rồi",
+    "...",
+    "..",
+    ".",
+    "Tao kêu hết rồi mà đợi quài!!!",
+    "...",
+    "Vậy nhen",
+    "...",
+    "Đi ngủ đây",
+  ],
+  /**
+   * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+   * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+   * 例如
+   * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+   *
+   * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+   * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+   */
+  imgs: {
+    Gái: "./imgs/countryside.png",
+    // "今天是你的生日": "./imgs/birthday.jpg",
+  },
+  // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+  desc: {
+    turn_on: "Nhấn cái này đi!",
+    play: "Rồi cái này nữa!",
+    bannar_coming: "Hơi sến ha! Rồi bật này lên",
+    balloons_flying: "Cái này hay nè",
+    cake_fadein: "Thêm cái này nữa",
+    light_candle: "Đốt nến đi!",
+    wish_message: "Xin nhật vuiii!!! Nhấn cái nữa xem",
+    story: "Rồi có tin nhắn nè",
+  },
+};
